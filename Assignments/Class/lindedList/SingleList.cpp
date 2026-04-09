c7ec2ef314fb0486b4084af0bd5cae2c5835a6f7
malik2322 <dasamalik18@gmail.com>
2026-04-09 16:24:41 -0500

    adding the Insertion funciton in SingleList.cpp file

--- Assignments/Class/lindedList/SingleList.cpp
+++ Assignments/Class/lindedList/SingleList.cpp
@@ -35,8 +35,55 @@ void traverseLinkedList(Node *head)
         }
         temp = temp->next_node;
     }
+    cout << endl;
+}
+// Make the first node of Linked List linked to the new node
+
+// Remove the head from the original first node of Linked List
+
+// Make the new node as the Head of the Linked List.
+Node *insertAtFront(Node *head, double x)
+{
+    Node *new_node = new Node(x);
+    new_node->next_node = head;
+    return new_node;
 }
 
+// Following is the approach to add a new node at the end of the linked list:
+
+// Create a new node and set its next pointer as NULL since it will be the last node.
+// Store the head reference in a temporary variable
+// If the Linked List is empty, make the new node as the head and return
+// Else traverse till the last node
+// Change the next pointer of the last node to point to the new node
+
+Node *insertAtEnd(Node *head, double x)
+{
+    Node *new_node = new Node(x);
+
+    // If the Linked List is empty, make
+    // the new node as the head and return
+    if (head == nullptr)
+    {
+        return new_node;
+    }
+
+    // Store the head reference in a temporary variable
+    Node *last = head;
+
+    // Traverse till the last node
+    while (last->next_node != nullptr)
+    {
+        last = last->next_node;
+    }
+
+    // Change the next pointer of the last node
+    // to point to the new node
+    last->next_node = new_node;
+
+    // Return the head of the list
+    return head;
+}
 int main()
 {
     // creating the first node(head of list)
@@ -56,6 +103,15 @@ int main()
     //     cout << temp->data_value << " ";
     //     temp = temp->next_node;
     // }
+    traverseLinkedList(head);
+
+    head = insertAtFront(head, 15);
+    cout << "after insertAtFront" << endl;
+    traverseLinkedList(head);
+
+    cout << "after insertAtEnd" << endl;
+
+    head = insertAtEnd(head, 25);
     traverseLinkedList(head);
 
     return 0;
